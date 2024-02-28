3b0a719ca89384e7dc0a8a41c72fd66bc9c03511
ahfyan <161601382+ahfyan@users.noreply.github.com>
2024-02-28 20:44:59 +0700

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ date_default_timezone_set("Asia/Jakarta");
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost:8080/portfolio';
+$config['base_url'] = 'http://ahfyan.github.io';
 
 /*
 |--------------------------------------------------------------------------
